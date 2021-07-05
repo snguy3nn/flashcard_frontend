c3189e36d3bc55c068e20547d9cfa9790c6cdcb6
Stephen Nguyen <snguy3nn@gmail.com>
2021-07-05 14:17:46 -0500

    Connected to api using axios

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,22 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
-    state = {  }
+    state = {
+        collection: [],
+        subject: [],
+        question: [],
+        answer: [],
+      }
+    
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/flashcard/')
+        .then(res => {
+            const flashcard = res.data;
+            this.setState({ flashcard });
+        })
+    }
+    
     render() { 
         return (
             <div>
